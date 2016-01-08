b6eaf9d0a6e42001c49340f051fd3dd9e32789a6
martenl <martenjanlosansky@gmail.com>
2016-01-08 05:30:29 +0100

    Deleted empty class body

--- src/main/scala/de/martenl/interview/util/datastructures/Graph.scala
+++ src/main/scala/de/martenl/interview/util/datastructures/Graph.scala
@@ -1,10 +1,6 @@
 package de.martenl.interview.util.datastructures
 
 /**
- * Created by Marten on 24.01.2015.
+ * Created on 24.01.2015.
  */
-class Graph(val vertices : List[String],val edges:Array[Array[Double]]) {
-
-
-
-}
+class Graph(val vertices : List[String],val edges:Array[Array[Double]])
